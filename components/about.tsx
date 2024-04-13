109bfe4159def61818f679ae5cbce63233cc4b24
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2024-04-13 21:08:45 +0530

    fixed the typos and mistakes in the about section

--- components/about.tsx
+++ components/about.tsx
@@ -8,21 +8,22 @@ export function About() {
         <p>
           Hey, I'm <span className="font-bold">Khushal Bhardwaj</span> a Full
           Stack Web Developer based in Jaipur, India. Passionate about building
-          web apps with React/NextJS for fast-growing startups. I'm also an
-          undergrad student at VIT Bhopal University.
+          web apps with React/NextJS. I'm also an undergrad at VIT Bhopal
+          University.
         </p>
         <Separator className="my-4" />
         <div className="flex flex-col gap-4">
           <p>
-            Other than programming, I write blogs. I have developed an hobby of
+            Other than programming, I write blogs. I've developed a hobby of
             writing blogs, mostly technical. Although I may not be following it
-            that regularly you might think but I do love it.
+            that regularly, you might think, but I do love it.
           </p>
           <p>
             I developed this habit of writing mainly because I was forced to
             under my academic curriculum, so rather than taking it as a burden I
-            quite enjoy it. I've developer this unique or I would rather say
-            writing with personality.
+            quite enjoy it. I've developed this unique or I would rather say
+            "writing with personality". I know these are big words for literally
+            technical blogging, but I feel like writing it 😜.
           </p>
         </div>
       </div>
